1a52ea912715af7f69e479a061632797e2eb4efa
doyonu <doyonu@educ.college-montreal.qc.ca>
2023-09-29 15:19:58 -0400

    l

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#Ulysse Doyon
 import random
 
+#définir les bornes (fonction)
 def bornes():
     minimum = int(input("définissez les bornes, minimum: "))
     maximum = int(input("définissez les bornes, maximum: "))
@@ -7,15 +9,18 @@ def bornes():
     print("J’ai choisi un nombre au hasard entre %d et %d."
         "\nÀ vous de le deviner..."% (minimum, maximum))
     return aleatoire
+#premier essai
 chiffre_aleatoire = bornes()
 essai = int(input("Entrez votre essai :"))
 
+#boucle d'essai (while)
 nb_essais = 0
 boucle = True
 while boucle:
     if essai == chiffre_aleatoire:
         print("bravo! Vous avez terminé avec %d essais" %(nb_essais))
         boucle = False
+#fin
 
     elif essai > chiffre_aleatoire:
         print("chiffre plus petit")
