dc8047c5895e3615d130e1c0afa9af16941612a0
doyonu <doyonu@educ.college-montreal.qc.ca>
2023-10-13 11:44:44 -0400

    l

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Ulysse Doyon
 import random
+choix = "oui"
+nb_essais = 1
 
 #définir les bornes (fonction)
 def bornes():
@@ -9,24 +11,33 @@ def bornes():
     print("J’ai choisi un nombre au hasard entre %d et %d."
         "\nÀ vous de le deviner..."% (minimum, maximum))
     return aleatoire
+
 #premier essai
 chiffre_aleatoire = bornes()
 essai = int(input("Entrez votre essai :"))
 
 #boucle d'essai (while)
-nb_essais = 0
-while essai != chiffre_aleatoire:
+while choix == "oui":
 
+#chiffre trop grand
     if essai > chiffre_aleatoire:
         print("chiffre plus petit")
         essai = int(input("nouveau essai:"))
         nb_essais += 1
 
+#chiffre trop petit
     elif essai < chiffre_aleatoire:
         print("chiffre plus grand")
         essai = int(input("nouveau essai:"))
         nb_essais += 1
 
     else:
-        pass
-print("bravo! Vous avez terminé avec %d essais" % (nb_essais))
+        print("bravo! Vous avez terminé avec %d essais" % (nb_essais))
+#choix recommencer
+        choix = input("voulez-vous recommencer?")
+        if choix == "oui":
+            chiffre_aleatoire = bornes()
+            essai = 0
+        else:
+            pass
+print("au revoir")
